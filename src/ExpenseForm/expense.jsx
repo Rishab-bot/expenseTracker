3090b58018387193e6628e818b33fa8242fc00f6
unknown <prataplavi@gmail.com>
2025-01-12 04:24:03 +0530

    changes pushed

--- src/ExpenseForm/expense.jsx
+++ src/ExpenseForm/expense.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import '../App.css';
 
 export function ExpenseMonitor({ balance, updateBalance }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
--- src/balance/balance.jsx
+++ src/balance/balance.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import '../App.css';
 
 export function BalanceDisplay({ balance, updateBalance }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
